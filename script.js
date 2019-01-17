f98043146da8e9db59757528376b6041ebddb48d
nathan sepulveda <nathans.composer@gmail.com>
2019-01-17 09:06:47 -0600

    trying things out

--- script.js
+++ script.js
@@ -1,66 +1,58 @@
-// alert("You get to choose five words")
-// let first = prompt("1. Give me a word")
-// let second = prompt("2. Give me another")
-// let third = prompt("3. Give me another")
-// let fourth = prompt("4. Give me another")
-// let fifth = prompt("5. Give me another")
 
 let counter = 0
 let answerArray = [];
 
 function questionaire() {
 
-for (let i = 0; i < 20; i++) {
-    let answers = answerArray[i]
-    answers = prompt("Give me a word")
-    // answers = answers + " ";
-    answerArray.push(answers) 
-    counter+= 1
-    console.log(answerArray)  
-    if (answers === ".") {
-        answerArray.push(answers) 
-        break
-    } 
-    // if (counter === 8) {
-    //     break
-    // }
-}
+    for (let i = 0; i < 20; i++) {
+        let answers = answerArray[i]
+        answers = prompt("Give me a word")
+        // answers = answers + " ";
+        answerArray.push(answers)
+        counter += 1
+        console.log(answerArray)
+        if (answers === ".") {
+            answerArray.push(answers)
+            break
+        }
+    }
 }
 
 questionaire();
-let messageArray = [];
-for (let i = 0; i < counter; i++) {
-    const messages = document.querySelector("#messages")
-const individualMessage = document.createElement('section')
-individualMessage.innerHTML = answerArray[i]
-individualMessage.className = "message"
-messageArray.push(individualMessage)
-    
-}
+
+console.log(answerArray)
+// var messagee = document.querySelector("#messages")
+// messagee.innerHTML = answerArray[2]
+// messagee.innerHTML += answerArray[1]
+// const individualMessage = document.createElement('section')
+
+// let messageArray = [];
+// for (let i = 0; i < counter; i++) {
+//     const messages = document.querySelector("#messages")
+//     // const individualMessage = document.createElement('section')
+//     individualMessage.innerHTML += answerArray[i]
+//     individualMessage.className = "message"
+//     messageArray.push(individualMessage)
+
+// }
+
+// console.log(messageArray)
 
 // const messages = document.querySelector("#messages")
 // const individualMessage1 = document.createElement('section')
 // individualMessage1.innerHTML = answerArray[0]
 // individualMessage1.className = "message"
-// const individualMessage2 = document.createElement('section')
-// individualMessage2.innerHTML = answerArray[1]
-// individualMessage2.className = "message"
-// const individualMessage3 = document.createElement('section')
-// individualMessage3.innerHTML = answerArray[2]
-// individualMessage3.className = "message"
-// const individualMessage4 = document.createElement('section')
-// individualMessage4.innerHTML = answerArray[3]
-// individualMessage4.className = "message"
-// const individualMessage5 = document.createElement('section')
-// individualMessage5.innerHTML = answerArray[4]
-// individualMessage5.className = "message"
 
 
-function messagePrint(mesaggess) {  
-    messages.appendChild(mesaggess)
-}
 
-// let messageArray = [individualMessage1, individualMessage2, individualMessage3, individualMessage4, individualMessage5]
+// function messagePrint(mesaggess) {
+//     document
+//     messages.appendChild(mesaggess)
+// }
+
+// var messagee = document.querySelector("#messages")
+// messagee.innerHTML = answerArray[2]
+// messagee.innerHTML += answerArray[1]
 
 
 let timerArray = []
@@ -69,18 +61,49 @@ let j = 0
 for (let i = 0; i < counter; i++) {
     timerArray.push(j)
     let k = Math.random() * 100
-    j+= 500 + k
+    j += 500 + k
     console.log(timerArray)
-    
+}
+
+// for (var i = 0; i < messageArray.length; i++) {
+
+//     let interval = timerArray[i]
+//     const currentMessage = messageArray[i]
+
+//     setTimeout(() => { messagePrint(currentMessage) }, interval)
+
+// }
+// function messagePrint(messagee, iterable) {
+//     var messagee = document.querySelector("#messages")
+//     let actualMessage = messagee.innerHTML
+//     // messagee.innerHTML = answerArray[iterable]
+//     actualMessage += answerArray[iterable]
+//     }
+
+for (var i = 0; i < answerArray.length; i++) {
+function printer() {
+   let paragraph = document.querySelector("messages")
+   paragraph.innerHTML = answerArray[2]
+}
+printer()
 }
 
 
 
-for (var i = 0; i < messageArray.length; i++ ) {
+// for (var i = 0; i < answerArray.length; i++) {
 
-    let interval = timerArray[i]
-    const currentMessage = messageArray[i]
-    
-    setTimeout(() => {messagePrint(currentMessage)}, interval)
-    
-}
+//     let interval = timerArray[i]
+//     const currentMessage = answerArray[i]
+//     var messagee = document.querySelector("#messages")
+
+//     setTimeout(() => { messagePrint(currentMessage, i) }, interval)
+
+// }
+
+
+// function messagePrint(messagee, iterable) {
+// var messagee = document.querySelector("#messages")
+// let actualMessage = messagee.innerHTML
+// // messagee.innerHTML = answerArray[iterable]
+// actualMessage += answerArray[iterable]
+// }
